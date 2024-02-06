704a8b7d8a5ff9863471ef554b961a6d868560d2
IremOztimur <iremoztimur07@gmail.com>
2024-02-06 23:53:34 +0300

    added strcmp file

--- library/libft/libft.h
+++ library/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: iremoztimur <iremoztimur@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 15:29:06 by ioztimur          #+#    #+#             */
-/*   Updated: 2024/02/06 22:34:06 by iremoztimur      ###   ########.fr       */
+/*   Updated: 2024/02/06 23:52:37 by iremoztimur      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,5 +85,6 @@ void		ft_matrix_delete(void *matrix, void (*del)(void *));
 size_t		ft_matrix_size(void *matrix);
 void		*ft_matrix_append(void *matrix, void *data, void *(*copy)(void *));
 void		*ft_matrix_new(size_t lines, size_t columns);
+int			ft_strcmp(char *s1, char *s2);
 
 #endif
