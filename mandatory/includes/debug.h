e9c6710deab65f7e93df3f10fbc8e59b6a3c40fa
IremOztimur <iremoztimur07@gmail.com>
2024-02-09 14:31:14 +0300

    added functions to debug

---
+++ mandatory/includes/debug.h
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iremoztimur <iremoztimur@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/08 20:46:12 by iremoztimur       #+#    #+#             */
+/*   Updated: 2024/02/08 21:53:27 by iremoztimur      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef DEBUG_H
+# define DEBUG_H
+
+# include "miniRT.h"
+
+/**
+ * @brief Prints in the standard output all the values that represent the
+ * ambient light in the scene.
+ *
+ * @param s The t_light struct (representing ambient light) to debug
+ */
+void	ambient_print(Light *s);
+
+/**
+ * @brief Prints in the standard output all the values related to the
+ * scene's camera.
+ *
+ * @param s The Camera struct to debug
+ */
+void	camera_print(Camera *s);
+
+/**
+ * @brief Prints in the standard output all the values related to a scene's
+ * light source.
+ *
+ * @param l The t_light struct (representing a light source) to debug
+ */
+void	light_print(Light *l);
+
+/**
+ * @brief Prints in the standard output all the values related to a scene's
+ * sphere.
+ *
+ * @param s The t_sphere struct to debug
+ */
+void	sphere_print(Sphere *s);
+
+/**
+ * @brief Redirects the program flow to one of the other print functions,
+ * since shape represents all the possible shapes. It also prints a shapes's
+ * ID.
+ *
+ * @param shape The t_shape struct to debug
+ */
+void	shape_print(t_shape *shape);
+
+/**
+ * @brief Prints in the standard output all the values related to a color.
+ *
+ * @param color The Color struct to debug
+ */
+void	color_print(Color *color);
+
+/**
+ * @brief Prints in the standard output all the values related to a 3D vector.
+ *
+ * @param vec The t_vec3 struct to debug
+ */
+void	vector_print(Vector vec);
+
+/**
+ * @brief Prints in the standard output all the values related to the world.
+ * This includes most of the other print functions, since Scene is the main
+ * struct.
+ *
+ * @param world A pointer to the Scene struct
+ */
+void	scene_print(Scene *scene);
+
+#endif
---
+++ mandatory/sources/debug/debug.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iremoztimur <iremoztimur@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/09 14:15:58 by iremoztimur       #+#    #+#             */
+/*   Updated: 2024/02/09 14:16:05 by iremoztimur      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "miniRT.h"
+
+void	ambient_print(Light *s)
+{
+	printf("Ambient:\n");
+	printf("  Ratio: (%.02f)\n", s->ratio);
+	printf("  Color: (%d, %d, %d)\n", s->color.r, s->color.g, s->color.b);
+}
+
+void	camera_print(Camera *c)
+{
+	printf("Camera:\n");
+	printf("  Center: (%.4lf, %.4lf, %.4lf)\n", \
+		c->center.x, c->center.y, c->center.z);
+	printf("  Normal: %.4lf, %.4lf, %.4lf\n", \
+		c->normal.x, c->normal.y, c->normal.z);
+	printf("  Fov: %2.f\n", c->fov);
+}
+
+void	light_print(Light *l)
+{
+	printf("Lightsource:\n");
+	printf("  Point: (%.4lf, %.4lf, %.4lf)\n", \
+		l->center.x, l->center.y, l->center.z);
+	printf("  Ratio: %.4lf\n", l->ratio);
+	printf("  Color: (%d, %d, %d)\n", l->color.r, l->color.g, l->color.b);
+}
+
+void	color_print(Color *color)
+{
+	printf("#%02X%02X%02X\n", color->r, color->g, color->b);
+}
+
+void	vector_print(Vector vec)
+{
+	printf("Vec: (x, y, z) = (%.4lf, %.4lf, %.4lf)\n", vec.x, vec.y, vec.z);
+}
+#include "miniRT.h"
+
+void	shape_print(t_shape *shape)
+{
+	if (shape->type == SPHERE)
+		sphere_print(&shape->data.sp);
+	printf("  ID: %d\n", shape->id);
+}
+
+void	sphere_print(Sphere *s)
+{
+	printf("Sphere:\n");
+	printf("  Center: (%.4lf, %.4lf, %.4lf)\n", \
+		s->center.x, s->center.y, s->center.z);
+	printf("  Radius: %.4lf\n", s->radius);
+	printf("  Color: (%d, %d, %d)\n", s->color.r, s->color.g, s->color.b);
+}
+
+void	ft_vector_print(t_vector *vector)
+{
+	uint32_t	i;
+
+	if (!vector)
+		return ;
+	i = -1;
+	while (++i < vector->size)
+		(*vector->print)(vector->array[i]);
+}
+
+void	scene_print(Scene *scene)
+{
+	printf("\n\t================ DEBUG ================\n\n");
+	ambient_print(&scene->ambient);
+	camera_print(&scene->camera);
+	ft_vector_print(scene->lights);
+	ft_vector_print(scene->shapes);
+	printf("Right Vector:\n  ");
+	vector_print(scene->right);
+	printf("Up Vector:\n  ");
+	vector_print(scene->up);
+	printf("Viewport:\n");
+	printf("  Height: %lf\n", scene->hview);
+	printf("  Width: %lf\n", scene->wview);
+	printf("\n\t=======================================\n\n");
+}
